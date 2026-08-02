e5fb54a27562df8a22fa0f7c0d0cf8d3beffbfe4
abhishek-virus <abhishekjha.virus@gmail.com>
2026-08-02 20:19:44 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& a) {
+        int n= a.size();
+        int cnt1=0, cnt2=0;
+        int el1=INT_MIN, el2=INT_MIN;
+        for(int i=0; i<n; i++){
+            if(cnt1 == 0 && el2 != a[i]){
+                cnt1 =1;
+                el1 = a[i];
+            }
+            else if(cnt2 == 0 && el1 != a[i]){
+                cnt2 = 1;
+                el2 = a[i];
+            }
+            else if(el1 == a[i]){
+                cnt1++;
+            }
+            else if(el2 == a[i]){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        vector<int> ls;
+        cnt1 =0, cnt2=0;
+        for(int i=0; i<n; i++){
+            if(el1 == a[i]) cnt1++;
+            if(el2 == a[i]) cnt2++;
+        }
+        int mini = (int)((n/3) + 1);
+        if(cnt1 >=mini) ls.push_back(el1);
+        if(cnt2>=mini) ls.push_back(el2);
+        return ls;
+
+    }
+};
