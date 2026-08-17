bb834ce0965b646508c017cea40d29d65c24b04e
abhishek-virus <abhishekjha.virus@gmail.com>
2026-08-17 08:37:14 +0530

    Add: search-in-rotated-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool search(vector<int>& a, int target) {
+        int n= a.size();
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(a[mid] == target) return true;
+            if(a[low] == a[mid] && a[mid] == a[high]){
+                low++;
+                high--;
+                continue;
+            }
+            else if(a[low] <= a[mid]){
+                if(a[low] <= target && target < a[mid]) high = mid - 1;
+                else low = mid + 1;
+            }
+            else{
+                if(a[mid] < target && target <= a[high]) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
